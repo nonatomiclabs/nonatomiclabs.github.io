27e8405d09ae3608e52bcf7411477c868feef348
Jean CRUYPENYNCK <Jean@Jeans-Mac-mini.local>
2018-02-23 15:48:52 +0100

    Fix HTTPS CSSs

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from pelicanconf import *
 
-SITEURL = 'https://nonatomiclabs.github.io'
+SITEURL = 'https://www.nonatomiclabs.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
